bfb33691196f5596e35f80c7e1bb3676951a18e4
lrodrin <lrodrin@gmail.com>
2022-08-30 12:12:51 +0200

    Version bump to 0.1.2

--- tool/__init__.py
+++ tool/__init__.py
@@ -19,4 +19,4 @@ __author__ = 'Laura Rodriguez-Navas <https://orcid.org/0000-0003-4929-1219>' \
              'Laia Codó-Tarraubella <https://orcid.org/0000-0002-6797-8746>'
 __copyright__ = '© 2020-2022 Barcelona Supercomputing Center (BSC), ES'
 __license__ = 'Apache 2.0'
-__version__ = '0.1.0'
+__version__ = '0.1.2'
